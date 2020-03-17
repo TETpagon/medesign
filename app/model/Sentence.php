f872ec3af9860a4ca600c44f73c8ea66838bc1cd
Slava <tetpagon@yandex.ru>
2020-03-17 10:30:20 +0300

    Добавил комментариев

--- app/model/Sentence.php
+++ app/model/Sentence.php
@@ -67,6 +67,7 @@ class Sentence extends \MeDesign\core\Model
         $matches = [];
         $patterns = [];
 
+        // поиск всех паттернов, и замена их условными ссылками типа [[\d]]
         do {
             preg_match_all("/{([^{]+?)}/", $expr, $matches);
             foreach ($matches[1] as $value) {
@@ -76,14 +77,16 @@ class Sentence extends \MeDesign\core\Model
             $patterns = array_merge($patterns, $matches[1]);
         } while (!empty($matches[1]));
 
+        // разделение паттернов на отдельные слова
         $splitPatterns = [];
         foreach ($patterns as $key => $value) {
             $splitPatterns[$key] = explode('|', $value);
         }
 
+        // последовательная замена уловных ссылок на конктретные слова,
+        // начиная с последних ссылок
         $sentences = [];
         $sentences[] = $expr;
-
         while (!empty($splitPatterns)) {
             $tmp = [];
             $key = array_key_last($splitPatterns);
